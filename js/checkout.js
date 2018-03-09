a551642a914227e7aa14d543d5082728d58900c4
INDEZENDE <i.rios.zendejas@gmail.com>
2018-03-09 11:29:17 -0600

    Agregando paypal con  total a pagar correcto

--- js/checkout.js
+++ js/checkout.js
@@ -1,8 +1,6 @@
 
-
-window.onload = () => {
-  getObjLocalStorage();//LLamando la inf almacenada en localStorage.
-}
+let json = getObjLocalStorage();//LLamando la inf almacenada en localStorage.
+console.log(json);
 
 
 function calculateTotal(array) {
@@ -22,12 +20,13 @@ arrayProducts[iEnd]= arrayProducts[iEnd].slice(0,-2);
 let jsonProduc = JSON.parse(strProduct);//Arreglo obtenido de localStorage.
 console.log(jsonProduc);
 paintCheckout(jsonProduc);
-let total = calculateTotal(jsonProduc);
-paintTotal (total);
-console.log(total);
+//let total = calculateTotal(jsonProduc);
+//paintTotal (total);
+//console.log(total);
+return jsonProduc
 
 }//fin de función getObjLocalStorage()
-
+/*
 function paintTotal (total) {
   let tableResult = document.querySelector('#table-result');//Elemento al que se le pintará la cuenta los productos agregados.
 
@@ -39,6 +38,9 @@ function paintTotal (total) {
 
   console.log(total);
 }
+*/
+
+
 
 function paintCheckout (array) {
   let tableCheck = document.querySelector('#table-checkout');//Elemento al que se le pintará la cuenta los productos agregados.
@@ -53,7 +55,11 @@ function paintCheckout (array) {
         `;
   }
 
-  let total =4000;
+  //let total =4000;
+  let total = calculateTotal(array);
+  console.log(total);
+  let strTotal = localStorage.setItem('total',total);
+
   let templateComplet = `
   <thead>
     <tr>
@@ -68,7 +74,7 @@ function paintCheckout (array) {
   <tr>
     <td></td>
     <td></td>
-
+    <td>${total}</td>
   </tr>
 
   </tbody>
@@ -76,3 +82,49 @@ function paintCheckout (array) {
 
   tableCheck.innerHTML = templateComplet;
 }//fin de función paintCheckout(array).
+
+let strTotal = localStorage.getItem('total');
+strTotal=parseInt(strTotal);
+console.log(strTotal);
+
+
+
+  paypal.Button.render({
+
+      env: 'sandbox', // sandbox | production
+
+      // PayPal Client IDs - replace with your own
+      // Create a PayPal app: https://developer.paypal.com/developer/applications/create
+      client: {
+          sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+          production: 'AXogq8X2C5u1kEiDR8P8KHsbQfS3YgiyxFd1Ovvjenv8nD-10pOhb4M9xOc_G6T1Adc3HKsdg5iEw1S9'
+      },
+
+      // Show the buyer a 'Pay Now' button in the checkout flow
+      commit: true,
+
+      // payment() is called when the button is clicked
+      payment: function(data, actions) {
+
+          // Make a call to the REST api to create the payment
+          return actions.payment.create({
+              payment: {
+                  transactions: [
+                      {
+                          amount: { total: `${strTotal}`, currency: 'USD' }
+                      }
+                  ]
+              }
+          });
+      },
+
+      // onAuthorize() is called when the buyer approves the payment
+      onAuthorize: function(data, actions) {
+
+          // Make a call to the REST api to execute the payment
+          return actions.payment.execute().then(function() {
+              window.alert('Payment Complete!');
+          });
+      }
+
+  }, '#paypal-button-container');
